38c83fd5e9387e92cabba4a4a6619ea5a89a55dd
erick260392 <erickliconamorales2016@gmail.com>
2022-12-06 05:57:04 -0600

    first commit

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    public function index(){
+        return 'WELCOME to page cursos thanks';
+    }
+
+    public function create(){
+        return "En esta pagina podras crear un curso";
+
+    }
+
+    public function show($curso){
+        return " Bienvenido al curso: $curso";
+
+    }
+}
